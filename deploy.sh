cbebf0a1b585e7da29e29c4943744d6dba6e3a27
yanuz93 <yanuzpurnomo@gmail.com>
2019-08-29 22:09:25 +0700

    coba lagi travis

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)" &&
-ssh-add -k ~/.ssh/id_rsa &&
-cd /var/www/helloworld
-git pull
-
-source ~/.profile
-echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
-docker stop helloworld
-docker rm helloworld
-docker rmi aprynur/helloworld
-docker run -d --name helloworld -p 5000:5000 aprynur/helloworld:latest
+docker stop hello
+docker rm hello
+docker rmi mathricks/react-front
+docker run -d --name hello -p 5000:5000 mathricks/react-front:sudah
 
