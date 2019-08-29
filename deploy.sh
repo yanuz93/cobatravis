3756fb9777d29a86506d62608fa780a4247ec000
yanuz93 <yanuzpurnomo@gmail.com>
2019-08-30 06:51:56 +0700

    coba

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 sudo docker stop hello
 sudo docker rm hello
 sudo docker rmi mathricks/react-front:sudah
-sudo docker run -d --name hello -p 5000:5000 mathricks/react-front:sudah
+sudo docker run -d --name hello -p 5000:80 mathricks/react-front:sudah
